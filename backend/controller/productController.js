5bccce0cf95f68275fbce50b38f1e5cb531e5276
Jayesh-11 <jayeshvyavahare132000@gmail.com>
2022-08-22 09:51:56 +0530

    Added feature to handle async error and unhandled rejections
    changes made to server.js and wrapped controller in catchAsyncError function

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,16 +1,17 @@
 // const { findById } = require("../models/productModel");
 const Product = require("../models/productModel");
 const ErrorHander = require("../utils/errorhander");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 
-exports.createProduct = async (req, res, next) => {
+exports.createProduct = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.create(req.body);
   res.status(201).json({
     success: true,
     product,
   });
-};
+});
 
-exports.updateProduct = async (req, res, next) => {
+exports.updateProduct = catchAsyncErrors(async (req, res, next) => {
   let product = await Product.findById(req.params.id);
   if (!product) {
     return next(new ErrorHander("Product not found,404"));
@@ -24,9 +25,9 @@ exports.updateProduct = async (req, res, next) => {
     success: true,
     product,
   });
-};
+});
 
-exports.deleteProduct = async (req, res, next) => {
+exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.findById(req.params.id);
   if (!product) {
     return next(new ErrorHander("Product not found,404"));
@@ -36,14 +37,14 @@ exports.deleteProduct = async (req, res, next) => {
     success: true,
     message: "deleted successfully",
   });
-};
+});
 
-exports.getAllProducts = async (req, res) => {
+exports.getAllProducts = catchAsyncErrors(async (req, res) => {
   const products = await Product.find();
   res.status(200).json({ success: true, products });
-};
+});
 
-exports.getProductDetails = async (req, res, next) => {
+exports.getProductDetails = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.findById(req.params.id);
   if (!product) {
     return next(new ErrorHander("Product not found,404"));
@@ -52,4 +53,4 @@ exports.getProductDetails = async (req, res, next) => {
     success: true,
     product,
   });
-};
+});
--- backend/server.js
+++ backend/server.js
@@ -9,3 +9,11 @@ connectDatabase();
 app.listen(process.env.PORT, () => {
   console.log(`server is working on http://localhost:${process.env.PORT}`);
 });
+
+process.on("unhandledRejection", (err) => {
+  console.log(`Error:${err.message}`);
+  console.log("server down due to unhandled promise rejection");
+  server.close(() => {
+    process.exit(1);
+  });
+});
