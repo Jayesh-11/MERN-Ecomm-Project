8e3001e6cd8b766fb3028b4266bec141e7fb278f
Jayesh-11 <jayeshvyavahare132000@gmail.com>
2022-08-22 10:05:12 +0530

    Added feature to handle cast error
    handles error like invalid id for product

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,8 +1,13 @@
-const ErrorHander = require("../utils/errorhander");
+const ErrorHandler = require("../utils/errorhander");
 
 module.exports = (err, req, res, next) => {
   err.statusCode = err.status || 500;
   err.message = err.message || "Internal server error";
+
+  if (err.name === "CastError") {
+    const message = `Resource not found. Invalid: ${err.path}`;
+    err = new ErrorHandler(message, 400);
+  }
   res.status(err.statusCode).json({
     success: false,
     message: err.message,
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,12 @@ const app = require("./app");
 const dotenv = require("dotenv");
 const connectDatabase = require("./config/database");
 
+process.on("uncaughtException", (err) => {
+  console.log(`Error:${err.message}`);
+  console.log(`server down due to Uncaught Exception`);
+  process.exit(1);
+});
+
 dotenv.config({ path: "backend/config/config.env" });
 
 connectDatabase();
